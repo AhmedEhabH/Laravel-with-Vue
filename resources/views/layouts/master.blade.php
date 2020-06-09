8accaccd49dc84198b31f62bdb2b2f52c4b37d84
AhmedEhabH <ahmed.ehab.h@gmail.com>
2020-06-09 16:58:42 +0200

    Fix 40 Fix 404 not found page

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,16 +6,16 @@ scratch. This page gets rid of all links and provides the needed markup only.
 -->
 <html lang="en">
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-  <!-- CSRF Token -->
-  <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <title>AdminLTE 3 | Starter</title>
+    <title>AdminLTE 3 | Starter</title>
 
-  <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body class="hold-transition sidebar-mini" >
 <div class="wrapper" id="app">
